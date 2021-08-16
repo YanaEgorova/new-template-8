f20ba4b930011952dc1a32c7e0bcdc320de9d681
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-16 20:47:28 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,300 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Fresh Food Feeder',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Feeder helps reduce the risk of choking; Only small pieces of digestible food get through.',
+            },
+            {
+                text: 'Simple and easy to use; Place food in mesh bag and snap shut to secure.',
+            },
+            {
+                text: 'Colored mesh reduces staining; Introduce fresh fruits and veggies to your baby with ease.',
+            },
+            {
+                text: 'BPA free.',
+            },
+            {
+                text: '6 plus months.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Quick Dry Hair Brush',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'How you brush your hair is the key to strong and healthy hair and the single most overlooked step in hair care. Hair is weakest when wet and pulling at it can snap it like a rubber band. This is why you should brush with a tool that gently loosens knots without pulling or breaking hair.',
+            },
+            {
+                text: 'Our hair brushes changed the hairbrush game, and transformed what used to be a painful and time consuming task into an effortless, fun and colorful part of your day.',
+            },
+            {
+                text: 'We offer our famous detangling bristles in lots of shapes and sizes – for travel, the shower, kids, styling and more.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Ball Pump with 5 needles',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Ball Pump with Ergonomic Design',
+                text: 'Made for your comfort with soft mold that fits perfectly in hand. Best as volleyball pump, basketball pump, soccer ball air pump.',
+            },
+            {
+                title: 'Push-Pull air delivery system',
+                text: 'Inflates when you push and when you pull, best of Soccer Ball Pumps.',
+            },
+            {
+                title: 'Forget about needles bending',
+                text: 'Unique soft needle plug system prevents needle from bending. This ball air pump is easy to use.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Rechargeable Grooming Kit',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Head and total body grooming.',
+            },
+            {
+                text: 'High carbon steel blades are precision ground to stay sharp longer.',
+            },
+            {
+                text: 'The right tools to define your style.',
+            },
+            {
+                text: 'Trim - shave - detail - outline - groom - touch up.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Thermos Stainless Steel Commuter Bottle',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Unbreakable 18/8 stainless steel interior and exterior withstand the demands of everyday use.',
+            },
+            {
+                text: 'Drink lid opens with push button ease and locks closed for leak-proof travel.',
+            },
+            {
+                text: 'Contoured body is comfortable to hold and fits most automobile drink holders.',
+            },
+            {
+                text: 'Ideal for hot and cold beverages.',
+            },
+            {
+                text: 'Ultra light design, weighs 210g',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Walker Brace/Walking Boot',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Specifically designed to provide mobility, protection and pneumatic support for day-to-day activities while recovering from injury.',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png'],
+    },
+    {
+        id: 'product-7',
+        name: '256GB Card',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'High-speed, Class 10 performance leverages UHS-I (U1 or U3 depends on capacity) technology for a read transfer speed up to 95MB/s (633x).',
+            },
+            {
+                text: 'Capture high quality images of stunning 1080p full-HD, 3D, and 4K video.',
+            },
+            {
+                text: 'Enjoy shooting longer without changing cards, large capacity options up to 1TB.',
+            },
+            {
+                text: 'High-speed file transfer from card to computer to dramatically accelerate workflow.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Raft',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Produced in limited quantities.',
+            },
+            {
+                text: 'The most comfortable pool lounger! Find yourself at the end of a rainbow with your head in the clouds. Oversized cloud pillow cradles your head for ultimate comfort and relaxation. Rainbow base floats your body to keep cool under the hot summer sun.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Hamster Habitat Cage',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'Beautiful Natural Wood Hamster Cage | Unique hamster cage is made with all natural, sustainable, liquid resistant wood & a robust wire net structure.',
+            },
+            {
+                text: 'Hamster Cage Includes All Accessories | It comes equipped with a plastic 5.5" exercise wheel, .04-gallon plastic water bottle, plastic food dish, hamster hide-out, 3 wood platforms & ladders.',
+            },
+            {
+                text: 'Easy Maintenance Wood Hamster Cage | A 2.6" deep plastic base contains bedding & can be removed to dump litter / etc. for routine cleaning, other plastic accessories & wire top can be cleaned with a mild, hamster-safe detergent, removable plastic base measures 22.75L x 12.25W x 2.6H inches.',
+            },
+            {
+                text: 'Convenient Front & Top Door Access | Simply lift the top door to gain access to your hamster & the accessories for cleaning or food / water refills.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png', './img/img9-4.png', './img/img9-5.png', './img/img9-6.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Infrared Heater',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'This is the perfect outdoor heating solution, featuring a weatherproof rating of IP35.',
+            },
+            {
+                text: 'Safer and cleaner to use. Does not output CO2 like propane or natural gas heaters that burn fossil fuels. No dangerous tanks, no plumbing and no ventilation needed. Mount horizontally or vertically.',
+            },
+            {
+                text: '6.5 foot long three pronged cord. This comes with a 1 year manufacturer warranty.',
+            },
+            {
+                text: 'NOTE: Will not mount on ceiling or turn on unless black box is mounted vertical. Tip-over safety switch will only allow to turn on when mounted to the wall. (no ceiling mounted operation).',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.jpg', './img/img10-3.png', './img/img10-4.jpg'],
+    },
+    {
+        id: 'product-11',
+        name: 'Tool Box & Mobile Work Center',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                text: 'Single front pull-up latch of the tool box with wheels easily opens up the unit.',
+            },
+            {
+                text: '4 work levels - top tool box and tray that can be placed on opened up lid , mid organizer section, large bottom bin for power tools make this portable tool box ideal to work from on site after having transported the equipment in it.',
+            },
+            {
+                text: 'Telescopic handle for ergonomic hold and easy maneuvering can be lowered for storage of the rolling tool box.',
+            },
+            {
+                text: 'Side handles allow to conveniently lift up above step on onto vehicle.',
+            },
+            {
+                text: 'Ball bearing slides for smooth opening even with heavy loads.',
+            },
+            {
+                text: 'Heavy duty wheels suitable for rough terrain',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
+    },
+    {
+        id: 'product-12',
+        name: '500-Channel Compact Handheld Scanner',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                title: 'Listen in and stay informed',
+                text: 'It is a very affordable 500 channel hand-held scanner with loads of features in a convenient compact design.',
+            },
+            {
+                title: 'Listen to over 32,000 frequencies',
+                text: 'Get started listening right away with convenient pre-sets for the most popular searches. Frequencies are preset in ten service banks, Non-Digital Police/Fire/Emergency, NOAA Weather reports, Marine, Racing, Civil Aircraft, Ham Radio, Railroad, CB Radio, and other bands. This makes it easy to find channels that interest you.',
+            },
+            {
+                title: 'PC programing',
+                text: 'lets you program your scanner using your PC. Close call RF capture technology instantly tunes to signals from nearby transmitters and the Priority Scan function Scans the channels you have designated as priority channels. Memory backup keeps the frequencies stored in memory for an extended time if the scanner loses power.',
+            },
+            {
+                title: 'Delay function',
+                text: 'Helps prevent missed replies during two-way conversations. Other features include keypad entry, Earphone Jack, key lock, battery low alert, battery save. The triple-conversion design ensures excellent Performance.',
+            },
+            {
+                title: 'Three power options',
+                text: '#1- The included USB cable will power your SR30C from your PC or other USB power source, such as a smart phone USB charger (not included). #2 - Two AA alkaline batteries (not included). #3 - Two AA rechargeable Ni-MH batteries (not included). You can also charge rechargeable Ni-MH batteries in the unit using the USB cable.',
+            },
+            {
+                title: 'Excellent entry level portable scanner',
+                text: 'This scanner is NOT for receiving DIGITAL communications (e.g. Project 25, DMR, NXDN). Some Police/Fire/Emergency/Public Safety Agencies in larger cities are switching over to DIGITAL Trunking communication systems. For Digital Scanners please look at other leading Uniden models such as the BCD436HP.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.png'],
+    },
+    {
+        id: 'product-13',
+        name: 'E-Book Reader',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                title: 'Advantage',
+                text: 'This product adopts E-Ink electronic ink screen with the advantages of readability, flexibility and low power consumption.',
+            },
+            {
+                title: 'Clearer',
+                text: 'Compared with other display technologies, E-Ink electronic Ink screen has better reflectivity and contrast ratio.',
+            },
+            {
+                title: 'As Comfortable As Reading A Paper Book',
+                text: 'They look like ink on paper, a medium that people feel comfortable reading and handling.',
+            },
+            {
+                title: '6 Inches Easy To Carry',
+                text: '6 inch screen, 189 g weight, you can easily grasp with one hand. Easy to carry, you can enjoy reading anytime and anywhere.',
+            },
+            {
+                title: 'Support Multiple Languages',
+                text: '29 languages to choose from. (Most countries: English, Chinese, Russian, Japanese, French, Spanish, Portuguese, Turkish, polish, Arabic, Italian, Dutch, German, etc.',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png', './img/img13-3.png', './img/img13-4.png'],
+    },
+]
